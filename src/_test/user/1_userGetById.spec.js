21521376b2c3729474c8bc6494f9ede0ea90fba9
e-nie <eniehaus111@gmail.com>
2024-08-26 01:39:50 +0200

    fix expect

--- src/_test/user/1_userGetById.spec.js
+++ src/_test/user/1_userGetById.spec.js
@@ -2,6 +2,7 @@ const { expect } = require('chai');
 const { gqlRequest } = require('../gqlRequest');
 const { userGetByIdQ, userCreateQ } = require('./queries');
 const { user } = require('./data');
+const User = require('../../models/User');
 
 let respData = null;
 let postData = null;
@@ -23,8 +24,14 @@ describe('USER GET BY ID', () => {
                     if (err) return done(err);
                     respData = res.body.data.userGetById;
                     console.log(respData);
+
                     expect(respData.firstName ).eq(user.userInput.firstName);
                     expect(respData.lastName ).eq(user.userInput.lastName);
+                    expect(respData._id ).eq(process.env.USER_ID);
+                    expect(respData).to.deep.equal({
+                        _id: process.env.USER_ID,
+                        ...user.userInput
+                    });
                     done();
                 });
         });
